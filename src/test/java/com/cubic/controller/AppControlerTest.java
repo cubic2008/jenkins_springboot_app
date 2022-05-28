35c17533ae0682e544477d9a3719c80233e88560
DG <it.college.101@gmail.com>
2022-05-28 18:57:31 -0400

    Initial version

---
+++ src/test/java/com/cubic/controller/AppControlerTest.java
@@ -0,0 +1,39 @@
+package com.cubic.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class AppControlerTest {
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	public void testGreeting() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("Greetings from Jenkins Spring Boot App!")));
+	}
+	
+	@Autowired
+	private TestRestTemplate template;
+
+    @Test
+    public void getHello() throws Exception {
+        ResponseEntity<String> response = template.getForEntity("/", String.class);
+        assertThat(response.getBody()).isEqualTo("Greetings from Jenkins Spring Boot App!");
+    }
+}
---
+++ src/test/java/com/cubic/controller/JenkinsSpringbootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cubic.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsSpringbootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
